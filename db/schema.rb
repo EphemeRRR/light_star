1600616c886fe6886f8436ca387097993856b4b1
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-27 18:08:28 +0200

    add sub category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_160241) do
+ActiveRecord::Schema.define(version: 2018_08_27_160650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_08_27_160241) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_histories_on_user_id"
     t.index ["video_id"], name: "index_histories_on_video_id"
+  end
+
+  create_table "sub_categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "super_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["super_category_id"], name: "index_sub_categories_on_super_category_id"
   end
 
   create_table "super_categories", force: :cascade do |t|
@@ -53,4 +61,5 @@ ActiveRecord::Schema.define(version: 2018_08_27_160241) do
 
   add_foreign_key "histories", "users"
   add_foreign_key "histories", "videos"
+  add_foreign_key "sub_categories", "super_categories"
 end
