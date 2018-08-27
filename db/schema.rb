d40d78d763793c65e3b4b3cc03a8c6b6adb99767
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-27 18:03:07 +0200

    add super category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_155740) do
+ActiveRecord::Schema.define(version: 2018_08_27_160241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_08_27_155740) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_histories_on_user_id"
     t.index ["video_id"], name: "index_histories_on_video_id"
+  end
+
+  create_table "super_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
