d88ca77d117db4ae0b9611bcda43ad0cdcf385fe
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-27 17:55:47 +0200

    did db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_071339) do
+ActiveRecord::Schema.define(version: 2018_08_27_133014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2018_08_25_071339) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "youtube_id"
+    t.integer "duration_seconds"
+    t.integer "minimum_age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
