d86b0f5b510b6df4c6d45e8040fb8edd8bb01134
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-28 12:21:30 +0200

    add simple show method

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,19 @@
 class VideosController < ApplicationController
+  before_action :set_video
+
   def index
   end
 
   def show
+    #
   end
 
   def search
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
   end
 end
