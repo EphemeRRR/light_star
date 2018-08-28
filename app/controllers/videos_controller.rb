e1b0b09c0c112b74035a2fa85357414bf9245034
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-28 12:34:42 +0200

    add skip_authenticate_user to video controller to allow non-users access

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,13 @@
 class VideosController < ApplicationController
+  skip_before_action :authenticate_user!
   before_action :set_video
 
   def index
+    if user_signed_in?
+      #
+    else
+      @videos = Video.all
+    end
   end
 
   def show
