2cbf2be8cb606e60ac16154b7975747fc0a24529
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-28 11:21:55 +0200

    generate video controller, routes, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'videos/:id', to: 'videos#show', as: 'video'
+  get 'videos/search', to: 'videos#search'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
