74ee08c2f37ed769fc8c180fa0e37002d58ccb77
Francois Marie Verdier <francoismarie.verdier@gmail.com>
2018-08-30 11:52:33 +0200

    validates video_category uniqueness cat scope video - video format 11char

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   has_many :histories, dependent: :destroy
   has_many :video_categories, dependent: :destroy
-  validates :youtube_id, uniqueness: true, presence: true
+  validates :youtube_id, uniqueness: true, presence: true, format: { with: /\A.{11}\z/ }
 
 end
--- app/models/video_category.rb
+++ app/models/video_category.rb
@@ -1,4 +1,7 @@
 class VideoCategory < ApplicationRecord
   belongs_to :video
   belongs_to :sub_category
+
+  validates :sub_category, uniqueness: { scope: :video }
+
 end
