31e16abde221f63ea6710f1fb6d1ae86b39768d2
Francois Marie Verdier <francoismarie.verdier@gmail.com>
2018-08-30 11:39:24 +0200

    add validations on subcategories and videos (presence and uniqueness on subcat->name and videos->youtube_id

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ApplicationRecord
   has_many :histories, dependent: :destroy
   has_many :video_categories, dependent: :destroy
+  validates :youtube_id, uniqueness: true, presence: true
+
 end
