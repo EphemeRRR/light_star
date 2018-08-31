a97e30ab6ae00cc561dadc5f1f454e9af2d2f8ad
Francois Marie Verdier <francoismarie.verdier@gmail.com>
2018-08-31 16:17:31 +0200

    add some videos, video descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_102836) do
+ActiveRecord::Schema.define(version: 2018_08_31_111853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,10 @@ ActiveRecord::Schema.define(version: 2018_08_30_102836) do
     t.integer "minimum_age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.boolean "verified"
+    t.boolean "pro"
+    t.boolean "introduction"
   end
 
   add_foreign_key "histories", "users"
