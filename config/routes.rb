e0d870e1572fa31c327aed2ab5c26e6c927ab2fb
christian <gudim87@hotmail.com>
2018-08-31 16:37:02 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'videos/search', to: 'videos#search'
   get 'videos/:id', to: 'videos#show', as: 'video'
   root to: 'videos#index'
-  post 'user/choose_categories', to: 'users#choose_categories'
 
   # get ':id' always last line of roots
   get 'user/:id', to: 'users#show', as: 'user_profile'
