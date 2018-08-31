9bb8e9a516311ad017f18c2fa32ce430ca991fe7
Jake Johnson <johnson.s.jake@gmail.com>
2018-08-31 15:16:49 +0200

    add fonctionality to get user choice data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,28 @@ class UsersController < ApplicationController
   def show
     @user = User.friendly.find(params[:id])
     @data = calculate_scores
-    @all_sub_categories = SubCategory.all
   end
 
-  def choose_categories
-    count = [0..16]
-    params['#{count}']
-    raise
+  def recieve_interest_choices
+    get_sub_categories(params)
   end
 
   private
+
+  def get_sub_categories(params)
+    # Define array to store parameters
+    params_array = []
+    # Grab only sub_category parameters
+    params.each { |param| params_array << param }
+    # Each parameters was an array, now take only first value
+    # of array which is the sub_category_id
+    sub_category_ids = params_array[2..-4].map { |array| array[0] }
+    augment_user_scores(sub_category_ids)
+  end
+
+  def augment_user_scores(user_score_array)
+
+  end
 
   def calculate_scores
 
