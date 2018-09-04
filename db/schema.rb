f1e1fe0143fbf494a1ff9f8becd0aa82f810bf2b
Jake Johnson <johnson.s.jake@gmail.com>
2018-09-04 18:07:36 +0200

    add default profile pic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_090515) do
+ActiveRecord::Schema.define(version: 2018_09_04_155554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2018_09_04_090515) do
     t.string "username"
     t.string "slug"
     t.boolean "admin", default: false, null: false
+    t.string "photo_id", default: "pfodkurg3t9w40qnseqe"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
