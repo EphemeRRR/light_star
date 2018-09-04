41627db977286dd062811fbe25d8a0aaedd0bd62
Christian Rognstad <gudim87@hotmail.com>
2018-09-04 10:08:05 +0200

    Added commentaire but not finished

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,7 @@ class Video < ApplicationRecord
   has_many :video_categories, dependent: :destroy
   has_many :sub_categories, through: :video_categories
   has_many :super_categories, through: :sub_categories
+  has_many :comments, dependent: :destroy
 
   validates :youtube_id, uniqueness: true, presence: true, format: { with: /\A.{11}\z/ }
   validates :minimum_age, presence: true
