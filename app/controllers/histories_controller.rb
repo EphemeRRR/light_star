53f919a85967515bce9fcb90e177729a0fb378c9
Christian Rognstad <gudim87@hotmail.com>
2018-09-05 19:01:50 +0200

    added js for button

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -7,6 +7,11 @@ class HistoriesController < ApplicationController
     @history = History.find(params["history"]["id"])
     @history.note = params["history"]["note"]
     @history.save
-    redirect_to video_path(@history.video)
+
+
+    respond_to do |format|
+        format.html { redirect_to video_path(@history.video) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
   end
 end
